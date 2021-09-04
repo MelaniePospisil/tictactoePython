5b92f29f95e66cb1f997c9970f055847198ae37e
Melli <melanie.p98@icloud.com>
2021-09-04 19:24:36 +0200

    everything but the check for the win

--- CS101.py
+++ CS101.py
@@ -1,1 +1,66 @@
-field = [[[], [], []], [[], [], []], [[], [], []]
+import sys
+script_name = sys.argv[0]
+
+class TikTakToe:
+    def __init__(self):
+        self.field = ["(1)", "(2)", "(3)", "(4)", "(5)", "(6)", "(7)", "(8)", "(9)"]
+        self.turn = " X "
+        self.already_taken = []
+        print(" ")
+        print("Welcome to tictactoe")
+        print(" ")
+        print("Adress the squares with the following numbers:")
+        print(" ")
+        print("[1][2][3]")
+        print("[4][5][6]")
+        print("[7][8][9]")
+        print("  ")
+        print("It's " + self.turn +"'s turn")
+        print(" ")
+        cell = int(input("Okay my dear " + self.turn + ", which cell do you want? "))
+        self.set(cell)
+        print(" ")
+        
+
+
+    def print_game(self):
+        print("-------------------------")
+        print("")
+        print(self.field[0] + "  I  " + self.field[1] + "  I  " + self.field[2])
+        print("")
+        print("-------------------")
+        print("")
+        print(self.field[3] + "  I  " + self.field[4] + "  I  " + self.field[5])
+        print("")
+        print("-------------------")
+        print("")
+        print(self.field[6] + "  I  " + self.field[7] + "  I  " + self.field[8])
+
+    def set(self, num):
+        if num in self.already_taken or num<1 or num>9:
+            print("-------------------------")
+            print("  ")
+            print("This is already taken, stupid. Try another one.")
+            print("  ")
+        else:
+            self.already_taken.append(num)
+            self.field[num-1] = self.turn
+            if self.turn == " X ":
+                self.turn = " O "
+            else:
+                self.turn = " X "
+            self.print_game()
+            print("  ")
+        print("It's " + self.turn +"'s turn")
+        print(" ")
+        cell = int(input("So my dear " + self.turn + ", which cell do you want? "))
+        self.set(cell)
+        print(" ")
+
+    
+game_1 = TikTakToe()
+
+    
+
+
+
